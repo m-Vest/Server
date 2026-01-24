0d48071fcb64d2345f50afc707a96f1d52f50d7e
jeon <jyh0872@gmail.com>
2026-01-24 23:58:29 +0900

    first commit

---
+++ src/main/java/com/mvest/mvest/MvestApplication.java
@@ -0,0 +1,13 @@
+package com.mvest.mvest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvest/mvest/MvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvest.mvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
