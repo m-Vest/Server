e69a67192f63061b9d55cd82cfa8fc923bab95ae
jeon <jyh0872@gmail.com>
2026-01-25 01:17:24 +0900

    chore: 프로젝트 구조 설정

---
+++ service/asset/src/main/java/mvest/asset/AssetApplication.java
@@ -0,0 +1,11 @@
+package mvest.asset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AssetApplication.class, args);
+    }
+}
